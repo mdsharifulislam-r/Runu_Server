f27b2b9c2880b52ee3ea27c59161751d8fff29c6
MD Shariful Islam <masom3456@gmail.com>
2025-01-19 00:42:26 +0600

    h

--- validation/login.validation.ts
+++ validation/login.validation.ts
@@ -2,5 +2,6 @@ import { body } from "express-validator";
 
 export const loginValidation = [
     body('email').isEmail().withMessage("Email Is Required"),
+    //dfd
     body('password').isString().withMessage("Password is Required")
 ]
