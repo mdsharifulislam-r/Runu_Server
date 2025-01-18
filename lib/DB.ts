756b029c1d03c1dc095f1995fb070768f4180c74
MD Shariful Islam <masom3456@gmail.com>
2025-01-19 01:10:45 +0600

    update db

--- lib/DB.ts
+++ lib/DB.ts
@@ -1,9 +1,10 @@
 import mysql from 'mysql2/promise'
-
+import dotenv from "dotenv"
+dotenv.config()
 export const pool = mysql.createPool({
-    user:"root",
-    database:"runu",
-    host:"localhost",
-    password:""
+    user:process.env.MYSQL_ADDON_USER,
+    database:process.env.MYSQL_ADDON_DB,
+    host:process.env.MYSQL_ADDON_HOST,
+    password:process.env.MYSQL_ADDON_PASSWORD
 })
 
